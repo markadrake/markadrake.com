405beebf6fc8d2cdb8e4edbda2b724c68f458f39
Mark Drake <markusdrake@gmail.com>
2022-01-19 01:26:47 -0500

    wip

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 
 module.exports = function (eleventyConfig) {
 	const handlebars = require("handlebars");
+
+	handlebars.registerHelper("commalist", function (array, options) {
+		return array[0]
+			.split(",")
+			.reduce((items, item) => items += options.fn(item.trim()), "");
+	});
+
 	eleventyConfig.setLibrary("hbs", handlebars);
 
 	return {
